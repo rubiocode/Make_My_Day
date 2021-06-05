f818785706200e042119e9cfd993a80ca2300502
Loryn <lorynhaykus@yahoo.com>
2021-06-05 13:56:10 -0600

    got favorites working

--- Favorites/favorites.js
+++ Favorites/favorites.js
@@ -1,80 +1,42 @@
-//Images
-//Pulling from the favs or if there are no liked items create an empty array
-const myFavs = JSON.parse(localStorage.getItem("favs")) || [];
 
-//Api call and button function to get images
-fetch("www.imageAPI")
-    .then(res => res.json())
-    .then(data => {
-        //For each item in the arry append the image url to the container
-        data.arr.forEach(item => {
-            $(".container").append(`<img src=${item.imgURL} /><button data-images=${item.imgURL}>favoriteBtn</button`)
 
-//When favorites button is clicked, prevent the page from refreshing as default and create a variable that that holds the item.imgURL for the liked photo
-            $(".container").on("click", "button", function (event) {
-                event.preventDefault();
-                let favImage = $(this).attr("data-images")
-//Create an object of the liked images
-                let imageObject = {
-                    imageSrc: favImage,
+$(document).ready(function () {
+   
 
-                }
-                //Push the items in the image object to the myFavs object
-                myFavs.push(imageObject)
-                //Save to local storage 
-                localStorage.setItem("fav", JSON.stringify(myFavs))
+    favMemes = JSON.parse(localStorage.getItem("memes"));
+    for (let i = 0; i < favMemes.length; i++) {
+        console.log(favMemes[i]);
+        $("#favoriteMemes").append(`<img src=${favMemes[i]}>`);
 
-            });
-        });
-    });
+
+    };
+
+
+/*
+
+    favMemes = JSON.parse(localStorage.getItem(""));
+    for (let i = 0; i < .length; i++) {
+
+        favGif = JSON.parse(localStorage.getItem(""));
+        console.log(favGif);
+        $("#favoriteGifs").append(`<img src=${favGif}>`);
 
 
 
+        favMemes = JSON.parse(localStorage.getItem(""));
+        for (let i = 0; i < .length; i++) {
 
-//GIFS api call and click function
-fetch("www.gifAPI")
-    .then(res => res.json())
-    .then(data => {
-        data.arr.forEach(item => {
-            $(".container").append(`<img src=${item.gifURL} /><button data-gifs=${item.gifURL}>favorite</button`)
+            favQuote = JSON.parse(localStorage.getItem(""));
+            console.log(favQuote);
+            $("favoriteQuotes").append(`<img src=${favQuote}>`);
 
-            $(".container").on("click", "button", function (event) {
-                event.preventDefault();
-                let favGif = $(this).attr("data-gifs")
-
-                let gifObject = {
-                    gifSrc: favGif,
-                }
-
-                myFavs.push(gifObject)
-                localStorage.setItem("fav", JSON.stringify(myFavs));
-            });
-        });
-    });
+        };
 
 
+    };
+*/
+});
 
-//Text/Quotes api call and click function
-
-fetch("www.quotesAPI")
-    .then(res => res.json())
-    .then(data => {
-        data.arr.forEach(item => {
-            $(".container").append(`<img src= ${item.quoteURL}/><button data-quotes=${item.quotesURL}>favorite</button`)
-
-            $(".container").on("click", "button", function (event) {
-                event.preventDefault();
-                let favQuote = $(this).attr("data-quotes")
-
-                let quoteObject = {
-                    quoteSrc: favQuote,
-                }
-
-                myFavs.push(quoteObject)
-                localStorage.setItem("fav", JSON.stringify(myFavs));
-            });
-        });
-    });
 
 
 
