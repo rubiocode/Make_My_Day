35d6f9c4233741f9262b55f51dfdf17f6c775540
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-06-15 14:42:54 -0700

    redid homepage to showcase personal style, added css hover effects, and deleted js code not needed for this version

--- Home/home.js
+++ Home/home.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
-    //giphs section
-    let randomGifIndex = Math.floor(Math.random() * 51);
-    let initGifAPI = 'https://api.giphy.com/v1/gifs/search?q=happy&api_key=SDmOSEvkMENogpqY8E44IlGAsWmz1GAw';
-    $.ajax({
-        url: initGifAPI,
-        method: "GET"
-        
-        //applying GIF on the page
-        }).then(function(response) {
-        $(".randomGif").attr("src", response.data[(randomGifIndex)].images.fixed_height.url)
-        $(".randomGif").attr("data-downsized", response.data[(randomGifIndex)].images.downsized.url)
-        //set local storage
-        localStorage.setItem("img", response.data[(randomGifIndex)].images.fixed_height.url);
-    })
+// no js code necessary here
 
     
     
